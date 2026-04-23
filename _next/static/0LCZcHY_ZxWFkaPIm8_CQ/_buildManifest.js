b50a3c8addcf24b0269de99b541522e498b1c42d
ankit <akv869205@gmail.com>
2026-04-23 16:05:01 +0530

    order management project done

--- _next/static/0LCZcHY_ZxWFkaPIm8_CQ/_buildManifest.js
+++ _next/static/0LCZcHY_ZxWFkaPIm8_CQ/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/order-management-system//_next/:path+",
+        "destination": "/order-management-system/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
